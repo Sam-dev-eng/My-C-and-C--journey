dc658cd0b1f96eebe6b938d1c6e3c22c9fc4460b
Sam-dev-eng <samuelchukwusom360@gmail.com>
2025-09-21 21:07:15 +0100

    added largest and average

--- C-folder/sumAverageLargest/main.c
+++ C-folder/sumAverageLargest/main.c
@@ -5,11 +5,23 @@ int main()
 {
 
     int numOne , numTwo , numThree , sum;
-
     printf("Enter three numbers\n");
     scanf("%d" "%d" "%d",&numOne , &numTwo, &numThree);
     sum = numOne + numTwo + numThree;
+    float average = sum / 3;
+
+    int largest = numOne;
+    if (largest < numTwo){
+    largest = numTwo;
+    }
+    if(largest < numThree){
+    largest = numThree;
+    }
+
     printf("Their sum are %d\n",sum);
+    printf("Their Average is %f \n", average);
+    printf("The largest is %d", largest);
+
 
     return 0;
 }
